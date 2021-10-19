1b88cb821cd3a91b5524c5f8a10d8194b963730c
GamB1t123 <stoianovskiy@gmail.com>
2021-10-19 15:23:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -48,8 +48,8 @@
 
 
 <?php
-if (isset($_POST)) {
-echo '<br>';
+if(!empty($_POST)) {
+    echo '<br>';
     echo "<table cellpadding='6' width='200' border = 2>" . "<tr>" . "<th>Name</th>" . "<th>Password</th>" . "<th>Email</th>" .
         "<th>Birthday</th>" . "<th>Gender</th>" . "<th>Favorite Color</th>" . "</tr>";
     echo "<td>". $_POST['num']."</td>" . "<td>". $_POST['pass']."</td>" . "<td>". $_POST['email']. "<td>". $_POST['date']."</td>" .
